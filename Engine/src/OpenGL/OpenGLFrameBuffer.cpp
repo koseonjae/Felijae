558a4ec53d14e40795a02976c089975c898985f5
koseonjae <goop702@gmail.com>
2024-11-21 02:14:59 +0900

    이슈 수정 : texture->bind하면서 이전에 glActiveTexture() 해둔 것 때문에, 프레임버퍼로 쓸 텍스쳐가 텍스쳐 유닛으로 바인딩 되버림 -> 아예 텍스쳐 유닛은 나중에 하도록 변경하자

--- Engine/src/OpenGL/OpenGLFrameBuffer.cpp
+++ Engine/src/OpenGL/OpenGLFrameBuffer.cpp
@@ -12,8 +12,6 @@ void OpenGLFrameBuffer::initialize(std::shared_ptr<Texture> texture, GLuint atta
   GLuint handle;
   glGenFramebuffers(1, &handle);
   glBindFramebuffer(GL_FRAMEBUFFER, handle);
-
-  texture->bind();
   glFramebufferTexture2D(GL_FRAMEBUFFER, attachmentIdx, GL_TEXTURE_2D, texture->getHandle(), 0);
 
   if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
