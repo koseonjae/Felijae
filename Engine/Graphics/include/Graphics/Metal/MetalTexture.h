429c43b4ef0befda542083d0d4fcca8b776b6ac7
koseonjae <goop702@gmail.com>
2024-11-25 00:49:29 +0900

    Add default value to metal texture initialize

--- Engine/Graphics/include/Graphics/Metal/MetalTexture.h
+++ Engine/Graphics/include/Graphics/Metal/MetalTexture.h
@@ -10,11 +10,11 @@ namespace goala {
 
 class MetalTexture : public Texture {
  public:
-  void initialize(File path, bool lazyLoading) override;
+  void initialize(File path, bool lazyLoading = false) override;
 
-  void initialize(int width, int height, ImageFormat format, bool lazyLoading) override;
+  void initialize(int width, int height, ImageFormat format, bool lazyLoading = false) override;
 
-  void initialize(ImageData imageData, bool lazyLoading) override;
+  void initialize(ImageData imageData, bool lazyLoading = false) override;
 
   void bind() override;
 };
