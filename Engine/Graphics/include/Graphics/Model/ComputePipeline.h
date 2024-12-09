64b099e306c80f7f2c0662af9779bdf439e3e2c2
koseonjae <goop702@gmail.com>
2024-12-10 01:01:33 +0900

    Remove comments

--- Engine/Graphics/include/Graphics/Model/ComputePipeline.h
+++ Engine/Graphics/include/Graphics/Model/ComputePipeline.h
@@ -14,12 +14,6 @@ struct ComputeBufferDescription {
 struct ComputePipelineDescription {
   ShaderDescription shader;
   ComputeBufferDescription buffer;
-  // VertexBufferDescription vertexBuffer;
-  // std::vector<ShaderDescription> shaders;
-  // Rasterizer rasterizer{};
-  // OutputMerger outputMerger{};
-  // std::shared_ptr<Uniforms> uniforms;
-  // ImageFormat format;
 };
 
 class ComputePipeline {
@@ -27,14 +21,7 @@ public:
   explicit ComputePipeline(ComputePipelineDescription desc) : m_desc(std::move(desc)) {}
   virtual ~ComputePipeline() = default;
 
-  // const Buffer* getVertexBuffer() const { return m_vertexBuffer.get(); }
-  // Buffer* getVertexBuffer() { return m_vertexBuffer.get(); }
-  //
-  // const Uniforms* getUniforms() const { return m_desc.uniforms.get(); }
-  // Uniforms* getUniforms() { return m_desc.uniforms.get(); }
-
 protected:
   ComputePipelineDescription m_desc{};
-  // std::shared_ptr<Buffer> m_vertexBuffer;
 };
 } // namespace goala
