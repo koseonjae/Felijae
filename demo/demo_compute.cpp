acd791ed9e85f12520abb931866f39c6a41abe10
koseonjae <goop702@gmail.com>
2024-12-10 14:00:12 +0900

    buf2texture pipeline 생성을 함수로 추출

--- demo/demo_compute.cpp
+++ demo/demo_compute.cpp
@@ -13,6 +13,31 @@
 
 using namespace goala;
 
+namespace {
+constexpr int width = 4;
+constexpr int height = 10;
+constexpr int totalElements = width * height;
+}
+
+std::shared_ptr<ComputePipeline> createBuf2TexturePipeline(MetalDevice* device, std::vector<float> inputData, std::shared_ptr<Texture> outputTexture) {
+  ComputePipelineDescription pipelineDesc = {
+    .shader = {
+      .source = File("asset://shader/buf2texture.msl").read(),
+      .type = ShaderType::COMPUTE
+    },
+    .buffer = {
+      .data = std::move(inputData),
+      .width = width,
+      .height = height,
+    },
+    .uniforms = {width, height},
+    .textures = {outputTexture}
+  };
+  auto pipeline = device->createComputePipeline(std::move(pipelineDesc));
+  return pipeline;
+}
+
+
 int main() {
   File::registerPath(DEMO_DIR + std::string("/asset"), "asset://");
 
@@ -20,11 +45,6 @@ int main() {
   assert(device && "Failed to create device");
 
   auto commandQueue = device->createCommandQueue({});
-
-  // 행렬 크기 정의
-  constexpr int width = 4;
-  constexpr int height = 10;
-  constexpr int totalElements = width * height;
 
   std::vector<float> inputData(totalElements);
   for (size_t i = 0; i < totalElements; ++i)
@@ -46,24 +66,9 @@ int main() {
   };
   auto outputTexture = device->createTexture(textureDesc);
 
-  ComputePipelineDescription pipelineDesc = {
-    .shader = {
-      .source = File("asset://shader/compute_example.msl").read(),
-      .type = ShaderType::COMPUTE
-    },
-    .buffer = {
-      .data = std::move(inputData),
-      .width = width,
-      .height = height,
-    },
-    .uniforms = {width, height},
-    .textures = {outputTexture}
-  };
+  auto buf2texturePipeline = createBuf2TexturePipeline(device.get(), std::move(inputData), outputTexture);
+  auto mtlPipeline = SAFE_DOWN_CAST(MetalComputePipeline*, buf2texturePipeline.get());
 
-  auto pipeline = device->createComputePipeline(std::move(pipelineDesc));
-  auto mtlPipeline = SAFE_DOWN_CAST(MetalComputePipeline*, pipeline.get());
-
-  // 커맨드 버퍼와 인코더 생성
   auto commandBuffer = commandQueue->createCommandBuffer({});
   auto mtlCmdBuffer = SAFE_DOWN_CAST(MetalCommandBuffer*, commandBuffer.get());
   auto computeEncoder = mtlCmdBuffer->getCommandBuffer()->computeCommandEncoder();
@@ -73,7 +78,6 @@ int main() {
   commandBuffer->commit();
   commandBuffer->waitUntilCompleted();
 
-  // 결과 확인
   std::array<float, totalElements> outputBuffer{};
   MTL::Region region = MTL::Region::Make2D(0, 0, width, height);
   SAFE_DOWN_CAST(MetalTexture*, outputTexture.get())->getTextureHandle()->getBytes(outputBuffer.data(), width * sizeof(float), region, 0);
